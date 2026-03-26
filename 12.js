3ee1a39be9c1a96d41208aa6f4012aa9a80eb31a
morochrislegal <christian.moro@escola.pr.gov.br>
2026-03-26 10:19:25 -0300

    prova

---
+++ 12.js
@@ -0,0 +1,4 @@
+let num1 = 60;
+let resultado = (num1 > 50) ? "maior que 50" : "menor ou igual a 50";
+
+console.log(resultado); 
---
+++ 13.js
@@ -0,0 +1,4 @@
+let num = 10;
+let resultado = (num % 2 === 0) ? "par" : "impar";
+console.log(resultado); 
+
