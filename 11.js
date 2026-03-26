f84feaac517d48d279e177adbb22b32888fcd869
morochrislegal <christian.moro@escola.pr.gov.br>
2026-03-26 10:23:58 -0300

    prova

---
+++ 11.js
@@ -0,0 +1,9 @@
+let numero = 15; 
+
+if (numero % 3 === 0) {
+    console.log(numero + " é divisível por 3");
+} else {
+    console.log(numero + " não é divisível por 3");
+}
+
+
--- 9.js
+++ 9.js
@@ -1,4 +1,4 @@
-let num1 = 10
-let num2 = 10 + 10
+let num1 = 10;
+let num2 = 10 + 10;
 
-console.log(`O valor somado com 10 é ${num2} `)
+console.log(`O valor somado com 10 é ${num2}`)
